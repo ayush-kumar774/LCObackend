6cb14f033d15d75f7c7846c51f5829c580687db0
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2022-04-12 19:14:27 +0530

    Added swagger for writing documentation

--- socialapp/index.js
+++ socialapp/index.js
@@ -3,7 +3,15 @@ const format  = require('date-format');
 
 const app = express();
 
+//swagger docs related
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 4000 ;
+
 
 app.get("/", (req, res) => {
         res.status(200).send(`<h1>Hello World From Aayush!</h1>`);
@@ -27,7 +35,7 @@ app.get("/api/v1/twitter", (req, res) => {
                 date: format.asString("dd-MM-yyyy : (hh::mm::ss)", new Date())
         } ;
         res.status(200).json({twitterSocial});
- })
+})
 
  app.get("/api/v1/linkedin", (req, res) => {
         const linkedinSocial = {
@@ -37,7 +45,7 @@ app.get("/api/v1/twitter", (req, res) => {
                 date: format.asString("dd-MM-yyyy : (hh::mm::ss)", new Date())
         } ;
         res.status(200).json({linkedinSocial});
- })
+})
 
 app.get("/api/v1/:token", (req, res) => {
 	console.log(req.params.token);
