090ac70d474b8ae0aff26cfc25e00236db535f4c
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2022-06-08 08:15:58 +0530

    Updated port

--- learningauthsystem/index.js
+++ learningauthsystem/index.js
@@ -1,4 +1,4 @@
 const app = require('./app');
 const {PORT} = process.env; // process.env.PORT
 
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}...`));
+app.listen(PORT||4000, () => console.log(`Server is running at port ${PORT}...`));
