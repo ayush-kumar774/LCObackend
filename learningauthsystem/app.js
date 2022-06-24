27134a10ac44d3b63b00a635722f738ed81f8a86
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2022-06-24 18:53:00 +0530

    Added route for registering a user

--- learningauthsystem/app.js
+++ learningauthsystem/app.js
@@ -1,10 +1,29 @@
 require("dotenv").config();
 const express = require('express');
 
+const User = require('./model/user')
+
 const app = express();
 
 app.get("/" , (req, res) => {
         res.send("<h1>Hello from auth system</h1>");
+});
+
+app.post("/register", (req, res) => {
+        const {firstname, lastname, email, password } = req.body;
+
+        if (!(firstname && lastname && email && password)) {
+                res.status(400).send("Name, email and password fields are mandatory");
+                // return res.status(400).send("Name, email and password fields are mandatory"); above line and this line are same
+                // after res.send() none of the line gets executed.
+        }
+
+        const existingUser = User.findOne({ email });
+
+        if (existingUser) {
+                res.status(401).send("User already exists");
+        }
+
 });
 
 module.exports = app;
