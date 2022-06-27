247b9ab387ccf5f68a793bff4e8dab5f3544ff54
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2022-06-27 15:06:39 +0530

    Added user validation

--- learningauthsystem/app.js
+++ learningauthsystem/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require('./config/database').connect();
 const express = require('express');
 
 const User = require('./model/user')
@@ -9,7 +10,7 @@ app.get("/" , (req, res) => {
         res.send("<h1>Hello from auth system</h1>");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
         const {firstname, lastname, email, password } = req.body;
 
         if (!(firstname && lastname && email && password)) {
@@ -18,7 +19,7 @@ app.post("/register", (req, res) => {
                 // after res.send() none of the line gets executed.
         }
 
-        const existingUser = User.findOne({ email });
+        const existingUser = await User.findOne({ email }); // PROMISE
 
         if (existingUser) {
                 res.status(401).send("User already exists");
